81d7811810454fef332d3fc1214e204d429f67a4
lucasdefariasbezerra <lucasfbezerra@hotmail.com>
2020-03-04 02:27:40 -0300

    test webhooks again

--- src/main/java/com/auth/tokenserver/config/SecurityConfig.java
+++ src/main/java/com/auth/tokenserver/config/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.oauth2.provider.ClientDetailsService;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
 
+/* test web hooks */
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
